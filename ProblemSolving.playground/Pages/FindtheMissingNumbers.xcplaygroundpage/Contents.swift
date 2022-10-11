8e7cdb4757eeb05842ecc1f892c176e014859f4e
humzashahid-mobileDev <115074955+humzashahid-mobileDev@users.noreply.github.com>
2022-10-11 09:44:17 +0400

    find the missing number in array

--- ProblemSolving.playground/Pages/FindtheMissingNumbers.xcplaygroundpage/Contents.swift
+++ ProblemSolving.playground/Pages/FindtheMissingNumbers.xcplaygroundpage/Contents.swift
@@ -2,6 +2,26 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+//Create a function that accepts an array of unsorted numbers from 1 to 100 where zero or more
+//numbers might be missing, and returns an array of the missing numbers.
+//Sample input and output
+//If your test array were created like this:
+//Then your method should [7, 21, 26], because those are the numbers missing from the array.
 
-//: [Next](@next)
+func findTheMissingNumbers(input: [Int]) -> [Int] {
+    var completeArray = Array(1...10)
+    var missingNumbers = [Int]()
+    for obj in completeArray {
+        if input.contains(obj) {
+            //
+        } else {
+            missingNumbers.append(obj)
+        }
+    }
+    return missingNumbers
+}
+var data: [Int] = Array(1...10)
+data.remove(at: 1)
+data.remove(at: 2)
+data.remove(at: 3)
+findTheMissingNumbers(input: data)
